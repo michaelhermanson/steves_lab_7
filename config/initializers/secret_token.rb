91aaa329a7a96793ec8a7f8b9e163efcc688332a
bendycode <stephen@bendyworks.com>
2011-02-22 21:05:23 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StevesLab7::Application.config.secret_token = '6b55e07d527560a2c3579553b3d431a24742fd97e222d3be3b802d846f34eb45013982d8cae2c34603ff8cd5c4cdcdff49b5bc9b062686f3251948d4c5cfe807'
